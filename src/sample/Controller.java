f4095832b83f5ff261cf151eeb4b96576fa04795
mustafaferhatimamoglu <mustafaferhatimamoglu@gmail.com>
2021-05-02 12:25:33 +0300

    button click

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable
+{
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+    }
+    @FXML
+    private void test(ActionEvent event)
+    {
+        System.out.println("lollolol");
+    }
 }
